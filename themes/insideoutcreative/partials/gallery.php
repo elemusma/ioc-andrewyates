64be7314dad6ea33326d0afb4224614b1145811e
Efrain Lemus <efraintlemusm@gmail.com>
2023-01-02 09:39:22 -0700

    undo

--- themes/insideoutcreative/partials/gallery.php
+++ themes/insideoutcreative/partials/gallery.php
@@ -21,13 +21,12 @@ $galleryCounter++;
 echo '<div class="col-lg-3 col-md-4 col-6 col col-portfolio p-1 overflow-h">';
 echo '<div class="position-relative img-hover w-100 overflow-h">';
 
-// if($gallery){
-//     echo '<a href="' . wp_get_attachment_image_url($image['id'], 'full') . '" data-lightbox="image-set-' . $galleryCounter . '">';
-//     // echo '</a>';
-// } else {
+if($gallery){
+    echo '<a href="' . wp_get_attachment_image_url($image['id'], 'full') . '" data-lightbox="image-set-' . $galleryCounter . '">';
+    // echo '</a>';
+} else {
     echo '<a href="' . wp_get_attachment_image_url($image['id'], 'full') . '" data-lightbox="image-set-advanced">';
-
-// }
+}
 
 echo wp_get_attachment_image($image['id'], 'full','',['class'=>'w-100 image-intro-gallery','style'=>'height:250px;object-fit:cover;object-position:top;']);
 echo '</a>';
