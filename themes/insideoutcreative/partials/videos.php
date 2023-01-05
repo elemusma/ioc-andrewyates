843cc7012e6b4f09c3648ef50398d0b066d138c4
Efrain Lemus <efraintlemusm@gmail.com>
2023-01-05 13:07:56 -0700

    mobile optimizations

--- themes/insideoutcreative/partials/videos.php
+++ themes/insideoutcreative/partials/videos.php
@@ -24,7 +24,7 @@ if(have_rows('videos')):
     $link = get_sub_field('link');
     $videoCounter++;
     
-    echo '<div class="col-lg-4 col-md-6 p-md-1">';
+    echo '<div class="col-lg-4 col-md-6 p-md-1 mb-lg-2 mb-md-4">';
     
     if( $link ): 
     $link_url = $link['url'];
